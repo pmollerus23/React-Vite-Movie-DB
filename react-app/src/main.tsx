2f10eb967673d5d018a91ee840c9794347a4605d
pmollerus23 <pmollerus23@gmail.com>
2025-03-21 22:21:17 -0400

    Pushing latest profile user flow changes

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -1,19 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-// import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter } from "react-router-dom";
 import "./css/index.css";
 import { AuthProvider, useAuth } from "./contexts/UserContext"; // Assuming UserContext is exported from here
 
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-    <AuthProvider>
-    <App />
-    </AuthProvider>
-      
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
